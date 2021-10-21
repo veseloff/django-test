15c767fbe9cf31a0e838097a72c29616cb7b41ea
Андрей <andrewzvorygin@yandex.ru>
2021-10-21 14:52:03 +0500

    Создал модели командировки, поездки и отеля

--- business_trips_assistant/business_trips_assistant/urls.py
+++ business_trips_assistant/business_trips_assistant/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('user_profile.urls'))
-    path('railways/', include('railways_api.urls'))
+    path('account/', include('user_profile.urls')),
+    path('railways/', include('railways_api.urls')),
 ]
--- business_trips_assistant/user_profile/models.py
+++ business_trips_assistant/user_profile/models.py
@@ -1,16 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from railways_api.models import City, Station
 
+# transport : 0 - самолёт, 1 - поезд
 # Миграции не применены
+# Спросить про Внешние ключи на бизнес поездки
+
 class Trip(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    transport = models.CharField(max_length=50)
+    business_trip = models.ForeignKey(BusinessTrip, on_delete=models.CASCADE)
+    transport = models.IntegerField()
     price_ticket = models.FloatField(blank=True)
     flight_number = models.CharField(max_length=10, blank=True)
     train_number = models.CharField(max_length=10, blank=True)
     date_departure = models.DateField()
-    date_return = models.DateField()
-    city_from = models.CharField(max_length=150)
-    city_to = models.CharField(max_length=150)
+    date_arrival = models.DateField()
+    city_from = models.ForeignKey(City, on_delete=models.CASCADE)
+    city_to = models.ForeignKey(City, on_delete=models.CASCADE)
     station_from = models.CharField(max_length=250)
     station_to = models.CharField(max_length=250)
+
+
+class BusinessTrip(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    credit = models.IntegerField()
+    date_start = models.DateField()
+    date_finish = models.DateField()
+    trip_to = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    trip_back = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+
+class Hotel(models.Model):
+    business_trip = models.ForeignKey(BusinessTrip, on_delete=models.CASCADE)
+    link = models.URLField()
+    name = models.CharField(max_length=150)
+    adress = models.CharField(max_length=150)
+    price = models.FloatField()
+
+
+# class Credit(models.Model):
+#     business_trip = models.ForeignKey(BusinessTrip, on_delete=models.CASCADE)

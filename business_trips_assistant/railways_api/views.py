637c5db5f26ff013a027ca6b442a0c03fe610e12
Андрей <andrewzvorygin@yandex.ru>
2021-10-21 12:35:23 +0500

    Коммит для мастера

--- business_trips_assistant/railways_api/views.py
+++ business_trips_assistant/railways_api/views.py
@@ -14,18 +14,13 @@ def get_path_to_city(request):
     Returns: json файл со списком всех поездов
 
     """
-    # city_to = request.GET['cityTo']
-    # city_from = request.GET['cityFrom']
-    # station_to = request.GET['stationTo']
-    # station_from = request.GET['stationFrom']
-    # date = request.GET['date']
-    city_to = 'НИЖНИЙ ТАГИЛ'
-    city_from = 'ЕКАТЕРИНБУРГ'
-    station_to = 'null'
-    station_from ='Екатеринбург-Пассажирский'
-    code_station_to = 0
-    code_station_from = 2030000
-    date = '20.11.2021'
+    city_to = request.GET['cityTo'].upper()
+    city_from = request.GET['cityFrom'].upper()
+    station_to = request.GET['stationTo'].upper()
+    station_from = request.GET['stationFrom'].upper()
+    code_station_to = request.GET['codeStationTo']
+    code_station_from = request.GET['codeStationFrom']
+    date = request.GET['date']
     answer = get_trains(city_to=city_to,
                         city_from=city_from,
                         date=date,
@@ -46,8 +41,7 @@ def get_city_by_prefix(request):
     Returns: json файл со списком городов начинающихся на prefix
 
     """
-    # prefix = request.GET['prefix']
-    prefix = 'Ека'.upper()
+    prefix = request.GET['prefix'].upper()
     cities = City.objects.filter(city__startswith=prefix)
     stations_to_json = [{'city': e.city, 'cityCode': e.id} for e in cities]
     answer_json = json.dumps(stations_to_json, ensure_ascii=False).encode('utf-8')
@@ -63,10 +57,8 @@ def get_station_by_city(request):
     Returns:
         json файл со списком станций в конкретном городе
     """
-    # city = request.GET['city'].upper()
-    # code = int(request.GET['code'])
-    city = "ЕКАТЕРИНБУРГ"
-    code = 0
+    city = request.GET['city'].upper()
+    code = int(request.GET['code'])
     if code == 0:
         code = [city.id for city in City.objects.filter(city=city)].pop()
     stations = [{"station": station.station, "code": station.code}

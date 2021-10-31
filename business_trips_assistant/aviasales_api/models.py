b0a976af7310934c47e1abdb7ecf001f589a5fd3
Churkin-Kirill <kirill.churkin.02@mail.ru>
2021-10-31 20:06:03 +0500

    Убрал у моделей поле prefix (было ненужным)

--- business_trips_assistant/aviasales_api/models.py
+++ business_trips_assistant/aviasales_api/models.py
@@ -4,11 +4,9 @@ from django.db import models
 class City(models.Model):
     code = models.CharField(max_length=3, primary_key=True)
     city = models.CharField(max_length=250)
-    prefix = models.CharField(max_length=2)
 
 
 class Airport(models.Model):
     code = models.CharField(max_length=3, primary_key=True)
     airport = models.CharField(max_length=250)
     city = models.CharField(max_length=250)
-    prefix = models.CharField(max_length=2)
--- business_trips_assistant/aviasales_api/write_fixtures_cities.py
+++ business_trips_assistant/aviasales_api/write_fixtures_cities.py
@@ -1,18 +1,14 @@
 import json
 
-# python manage.py loaddata station.json чтобы применить фикстуры
+# python manage.py loaddata cities.json чтобы применить фикстуры
 data_to_json = []
 
 with open('D:\\Users\\Desktop\\cities.json', 'r', encoding='utf-8') as cities_json:
     cities_info = json.load(cities_json)
     for city_info in cities_info:
         if city_info['country_code'] == 'RU':
-            dict_json = {}
-            dict_json['model'] = 'aviasales_api.city'
-            dict_json['pk'] = city_info['code'].upper()
-            dict_json['fields'] = {'city': city_info['name'].upper(),
-                                   'prefix': city_info['name'][:2].upper()
-                                   }
+            dict_json = {'model': 'aviasales_api.city', 'pk': city_info['code'].upper(),
+                         'fields': {'city': city_info['name'].upper()}}
             data_to_json.append(dict_json)
 
 js_data = json.dumps(data_to_json, ensure_ascii=False, indent=4)

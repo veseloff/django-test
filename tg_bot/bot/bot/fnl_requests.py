07a974626d0623b85b87488a3e9e29bd3e477f78
PavelShershnoyv <pavel.shershnoyv.02@gmail.com>
2021-11-11 00:19:39 +0500

    updated handler and delete comments

--- tg_bot/bot/bot/fnl_requests.py
+++ tg_bot/bot/bot/fnl_requests.py
@@ -1,9 +1,5 @@
-import datetime
 import time as t
-# time_s = t.strptime('2021-11-02', "%Y-%m-%d")
-#
-# print(t.strftime('%d-%m-%Y', time_s)) -> #tuple is converted to string-format
-# print(time_s.tm_mday, time_s.tm_mon, time_s.tm_year)
+
 
 def json_parser(json):
     months = {
@@ -20,14 +16,13 @@ def json_parser(json):
         11: "ноября",
         12: "декабря"
     }
+
     date = json["operation"]["date"]
-    total_sum = json["operation"]["sum"]/100
+    total_sum = json["operation"]["sum"] / 100
     items = []
     for item in json["ticket"]["document"]["receipt"]["items"]:
-        items.append(f"* {item['name']} - {str(item['sum']/100)} \u20bd")  # + " " + str(item["sum"]/100) f"- {item["name"]} {str(item["sum"]/100)}}"
-    shop = json["ticket"]["document"]["receipt"]["retailPlace"]
+        items.append(f"* {item['name']} - {str(item['sum']/100)} \u20bd")
     time_s = t.strptime(date[:10], "%Y-%m-%d")
     time_res = f"{time_s.tm_mday} {months[time_s.tm_mon]} {time_s.tm_year} года"
     return time_res, items, total_sum
 
-# f"{time_res} Вы купили: {items}, потратив всего: {total_sum}"
--- tg_bot/bot/bot/handlers/handler.py
+++ tg_bot/bot/bot/handlers/handler.py
@@ -1,54 +1,33 @@
-import logging
 import requests
+import re
 from tg_bot.bot.bot.loader import dp
 from aiogram.types import Message
-import json
 from tg_bot.bot.bot.nalog_python import NalogRuPython
 from tg_bot.bot.bot.fnl_requests import json_parser
 
 
 @dp.message_handler(content_types=['document', 'photo'])
 async def handle_docs_photo(message: Message):
-    logging.info('зашёл в метод')
     if message.document is None:
         await message.photo[-1].download('чек1.jpg')
         filename = 'чек1.jpg'
     else:
         await message.document.download('чек2.jpg')
         filename = 'чек2.jpg'
-    data = parse_data(get_info_zxing_qrscanner(filename))
     client = NalogRuPython()
-    qr_code = get_info_zxing_qrscanner(filename).replace("amp;", '')
+    qr_code = get_info_zxing_qrscanner(filename)
     ticket = client.get_ticket(qr_code)
-    print(json.dumps(ticket, indent=4, ensure_ascii=False))
-    print(ticket)
-    # f"{time_res} Вы купили: {items}, потратив всего: {total_sum}"
     data_checka = json_parser(ticket)
     result_answer = ""
     for el in data_checka[1]:
         result_answer = result_answer + '\n' + el
     await message.answer(f"{data_checka[0]} Вы купили: {result_answer}  \n \n Потратив всего: {data_checka[2]} \u20bd")
-    #  await message.answer(json_parser(json.dumps(ticket, indent=4, ensure_ascii=False)))
-    value = data[0][6:]
-    month = data[0][4:6]
-    year = data[0][:4]
-
-    # await message.answer(f'{value}.{month}.{year} вы совершили покупку на {data[1]} рублей')
-    # logging.info('fff')
 
 
 @dp.message_handler()
 async def exo(message: Message):
     await message.answer(f'Ты написал {message.text}.\n Я жду команду /start')
     await message.photo[0].download()
-
-
-def parse_data(data):
-    date = data[2:10]
-    sum = data[22:25]
-    fn = data[37:52]
-    fp = data[71:80]
-    return date, sum, fn, fp
 
 
 def qrcode_scanner(url, filename):
@@ -66,16 +45,11 @@ def qrcode_scanner(url, filename):
 def get_info_zxing_qrscanner(filename):
     url = "https://zxing.org/w/decode"
     rr = qrcode_scanner(url, filename)
-
     s = rr["text"]
-    i = s.find("pre")
-    s = s[i + 4:]
-    i = s.find("pre")
-    s = s[:i]
-    s = s.strip("/")
-    s = s.strip("<")
-    s = s.strip(">")
-    return s
+    pattern = r't=[0-9T]+&amp;s=[0-9.]+&amp;fn=[0-9]+&amp;i=[0-9.]+&amp;fp=[0-9]+&amp;n=[0-9]+'
+    result = re.findall(pattern, s)[0].replace("amp;", '')
+
+    return str(result)
 
 
 

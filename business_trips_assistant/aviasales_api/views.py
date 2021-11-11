406360a7f7ea0c23228e75b5ab45e472c3521770
Churkin-Kirill <kirill.churkin.02@mail.ru>
2021-11-11 21:39:45 +0500

    Переделал форму ввода / вывода данных

--- business_trips_assistant/aviasales_api/views.py
+++ business_trips_assistant/aviasales_api/views.py
@@ -1,17 +1,20 @@
 """Модуль обрабатывает запросы с клиента, связанные с API Aviasales"""
-from django.shortcuts import render
 from .handler import get_request, get_information_flight
+from .models import City
+from django.http import HttpResponse
 
 
-def get_air_ticket(name_city_departure, name_city_arrival, depart_date, return_date):
+def get_air_ticket(request):
     """
     Метод находит информацию о самом дешёвом прямом авиарейсе туда-обратно с заданными параметрами
     Args:
-        name_city_departure: название города отправления на русском языке
-        name_city_arrival: название города прибытия на русском языке
-        depart_date: дата отправления в формате YYYY-MM-DD
-        return_date: дата возвращения в формате YYYY-MM-DD
+        request: Request содержит информацию о дате поездки, месте отправления и месте прибытия
     Returns: JSON файл с информацией о рейсе
     """
+    name_city_departure = request.GET.get('cityFrom')
+    name_city_arrival = request.GET.get('cityTo')
+    depart_date = request.GET.get('dateDepart')
+    return_date = request.GET.get('dateReturn')
     response = get_request(name_city_departure, name_city_arrival, depart_date, return_date)
-    return get_information_flight(response)
+    result = get_information_flight(response)
+    return HttpResponse(result, content_type='application/json', charset='utf-8')

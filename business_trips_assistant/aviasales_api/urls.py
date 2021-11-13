668499d88a1bf107e5a04a0d7727f462cd6cb723
Churkin-Kirill <kirill.churkin.02@mail.ru>
2021-11-13 22:40:47 +0500

    Добавил поиск городов по префиксу

--- business_trips_assistant/aviasales_api/urls.py
+++ business_trips_assistant/aviasales_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_air_ticket
+from .views import get_air_ticket, get_city_by_prefix
 
 urlpatterns = [
     path('ticket_avia/', get_air_ticket),
+    path('cities_list/', get_city_by_prefix),
 ]
--- business_trips_assistant/aviasales_api/views.py
+++ business_trips_assistant/aviasales_api/views.py
@@ -1,6 +1,7 @@
 """Модуль обрабатывает запросы с клиента, связанные с API Aviasales"""
 from .handler import get_request, get_information_flight
 from django.http import HttpResponse
+from .models import City
 
 
 def get_air_ticket(request):
@@ -10,10 +11,22 @@ def get_air_ticket(request):
         request: Request содержит информацию о дате поездки, месте отправления и месте прибытия
     Returns: JSON файл с информацией о рейсе
     """
-    name_city_departure = request.GET.get('cityFrom')
-    name_city_arrival = request.GET.get('cityTo')
+    name_city_departure = request.GET.get('cityFrom').upper()
+    name_city_arrival = request.GET.get('cityTo').upper()
     depart_date = request.GET.get('dateDepart')
     return_date = request.GET.get('dateReturn')
     response = get_request(name_city_departure, name_city_arrival, depart_date, return_date)
     result = get_information_flight(response)
     return HttpResponse(result, content_type='application/json', charset='utf-8')
+
+
+def get_city_by_prefix(request):
+    """
+    Метод находит города по переданному префиксу
+    Args:
+        request: Request содержит префикс города
+    Returns: json файл со списком городов начинающихся на prefix
+    """
+    prefix = request.GET.get('prefix').upper()
+    cities = [c.city + " " for c in City.objects.filter(city__startswith=prefix)]
+    return HttpResponse(cities, content_type='application/json', charset='utf-8')

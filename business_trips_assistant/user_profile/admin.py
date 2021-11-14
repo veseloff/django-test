b7033c7b857973efaaeeae3763bd8b04fd76c75c
Андрей <andrewzvorygin@yandex.ru>
2021-11-14 21:26:46 +0500

    Зрагистрирвоал модель чек в админке

--- business_trips_assistant/user_profile/admin.py
+++ business_trips_assistant/user_profile/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Trip, BusinessTrip, Hotel
+from .models import Trip, BusinessTrip, Hotel, Cheque
 
 # Register your models here.
 admin.site.register(BusinessTrip)
 admin.site.register(Trip)
 admin.site.register(Hotel)
+admin.site.register(Cheque)

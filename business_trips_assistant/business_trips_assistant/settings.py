23982bb7911f5a54c89f2df3e863eeb20a5e45ad
Андрей <andrewzvorygin@yandex.ru>
2021-11-24 21:44:33 +0500

    Обновление зависимостей

--- business_trips_assistant/business_trips_assistant/settings.py
+++ business_trips_assistant/business_trips_assistant/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'corsheaders',
     'railways_api.apps.RailwaysApiConfig',
     'user_profile.apps.UserProfileConfig',

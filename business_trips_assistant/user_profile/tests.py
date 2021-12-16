ae0d154bbd30068f9eccb09a18f86ecbac6d5235
Андрей <andrewzvorygin@yandex.ru>
2021-12-16 18:26:59 +0500

    Попытка написать первый тест

--- business_trips_assistant/user_profile/tests.py
+++ business_trips_assistant/user_profile/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        # User.objects.create_user(username='adrenalin', password='1adrenalin3')
+        self.client = Client()
+
+    def test_reg(self):
+        resp = self.client.post('account/registration', data={'email': 'adds@asd.ru', 'username': 'adrenalin',
+                                                              'password': 'adrenalin123', 'firstname': 'Василий',
+                                                              'lastname': 'Ретровчи'})
+        print(type(resp))
+        self.assertEqual(resp.status_code, 200)
